7c0196bb5c606ed4159fd7210c349e9ab666a933
LordEidi <hadrian@clibanarii.praetorians.ch>
2016-06-03 11:18:11 +0200

    Fixed field size for content and timezone fields in DB thanks to @mdarveau

--- libs/db.js
+++ libs/db.js
@@ -22,13 +22,13 @@ var sequelize = new Sequelize(config.db_name, config.db_uid, config.db_pwd, {
 var ICS = sequelize.define('ICS', {
     pkey: { type: Sequelize.STRING, allowNull: false, unique: true, primaryKey: true},
     calendarId: { type: Sequelize.STRING, allowNull: false},
-    content: { type: Sequelize.STRING, allowNull: false}
+    content: { type: Sequelize.TEXT, allowNull: false}
 });
 
 var CAL = sequelize.define('CAL', {
     pkey: { type: Sequelize.STRING, allowNull: false, unique: true, primaryKey: true},
     owner: { type: Sequelize.STRING, allowNull: false},
-    timezone: { type: Sequelize.STRING, allowNull: false},
+    timezone: { type: Sequelize.TEXT, allowNull: false},
     order: { type: Sequelize.STRING, allowNull: false},
     free_busy_set: { type: Sequelize.STRING, allowNull: false},
     supported_cal_component: { type: Sequelize.STRING, allowNull: false},
@@ -41,7 +41,7 @@ var VCARD = sequelize.define('VCARD', {
     pkey: { type: Sequelize.STRING, allowNull: false, unique: true, primaryKey: true},
     ownerId: { type: Sequelize.STRING, allowNull: false},
     addressbookId: { type: Sequelize.STRING, allowNull: false},
-    content: { type: Sequelize.STRING, allowNull: false},
+    content: { type: Sequelize.TEXT, allowNull: false},
     is_group: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false}
 });
 
