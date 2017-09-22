65269815aa09a1116d04a3a5c0af270ee89d2de1
LordEidi <>
2017-09-22 15:08:45 +0200

    Springcleaning

--- libs/db.js
+++ libs/db.js
@@ -13,6 +13,10 @@ var config = require('../config').config;
 
 var Sequelize = require('sequelize');
 
+/**
+ *
+ * @type {*|Promise.Sequelize|Sequelize}
+ */
 var sequelize = new Sequelize(config.db_name, config.db_uid, config.db_pwd, {
     host: config.db_host || 'localhost',
     dialect: config.db_dialect,
@@ -20,6 +24,11 @@ var sequelize = new Sequelize(config.db_name, config.db_uid, config.db_pwd, {
     storage: config.db_storage
 });
 
+/**
+ *
+ * @type {Model}
+ * represents an event object with a start and an end date
+ */
 var ICS = sequelize.define('ICS', {
     pkey: { type: Sequelize.STRING, allowNull: false, unique: true, primaryKey: true},
     calendarId: { type: Sequelize.STRING, allowNull: false},
@@ -28,6 +37,11 @@ var ICS = sequelize.define('ICS', {
     content: { type: Sequelize.TEXT, allowNull: false}
 });
 
+/**
+ *
+ * @type {Model}
+ * represents a calendar object containing events (ics)
+ */
 var CAL = sequelize.define('CAL', {
     pkey: { type: Sequelize.STRING, allowNull: false, unique: true, primaryKey: true},
     owner: { type: Sequelize.STRING, allowNull: false},
@@ -40,6 +54,11 @@ var CAL = sequelize.define('CAL', {
     synctoken: { type: Sequelize.INTEGER, allowNull: false, defaultValue: 0}
 });
 
+/**
+ *
+ * @type {Model}
+ * represents an address (vcard) object
+ */
 var VCARD = sequelize.define('VCARD', {
     pkey: { type: Sequelize.STRING, allowNull: false, unique: true, primaryKey: true},
     ownerId: { type: Sequelize.STRING, allowNull: false},
@@ -48,6 +67,11 @@ var VCARD = sequelize.define('VCARD', {
     is_group: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false}
 });
 
+/**
+ *
+ * @type {Model}
+ * represents an addressbook containing addresses (vcard)
+ */
 var ADDRESSBOOK = sequelize.define('ADB', {
     pkey: { type: Sequelize.STRING, allowNull: false, unique: true, primaryKey: true},
     ownerId: { type: Sequelize.STRING, allowNull: false},
@@ -55,17 +79,37 @@ var ADDRESSBOOK = sequelize.define('ADB', {
     synctoken: { type: Sequelize.INTEGER, allowNull: false, defaultValue: 0}
 });
 
+/**
+ *
+ * @type {Model}
+ * users are grouped into usergroup. this is the binding table between users -n--n- groups
+ */
 var USER_GROUP = sequelize.define('USER_GROUP', {
     userId: { type: Sequelize.STRING, allowNull: false, unique: false, primaryKey: true},
     groupId: { type: Sequelize.STRING, allowNull: false, unique: false, primaryKey: true},
     description: { type: Sequelize.TEXT, allowNull: true}
 });
 
+/**
+ *
+ * @type {Model}
+ * a group of users which can have specific authorisation rules and permissions.
+ * a group is somewhat a role in RBAC.
+ */
 var GROUP = sequelize.define('GROUP', {
     groupId: { type: Sequelize.STRING, allowNull: false, unique: false, primaryKey: true},
     description: { type: Sequelize.TEXT, allowNull: true}
 });
 
+/**
+ *
+ * @type {Model}
+ * a permission for a specific group. Permission has always this structure:
+ *
+ * entrypoint(card vs calendar):path:to:element:action
+ *
+ * Users need to have respective permission to execute specific action
+ */
 var PERMISSION = sequelize.define('PERMISSION', {
     permissionId: { type: Sequelize.STRING, allowNull: false, unique: true, primaryKey: true},
     groupId: { type: Sequelize.STRING, allowNull: false},
